eea6be8d2193ed43ff4ebc5c1289cfda605ad40d
Giuli <giulianmaccarone@gmx.net>
2022-02-09 11:38:39 +0100

    utf8 & reaction

--- DerMeister_Bot.py
+++ DerMeister_Bot.py
@@ -1,4 +1,4 @@
-import discord
+ï»¿import discord
 
 
 
@@ -7,7 +7,7 @@ class MyClient(discord.Client):
 
 
 
-    #Einloggen in die Konsole rückmeldung
+    #Einloggen in die Konsole rÃ¼ckmeldung
     async def on_ready(self):
         print("Login Successfully.")
 
@@ -19,44 +19,44 @@ class MyClient(discord.Client):
         if message.author == client.user:       #mcacht das der Bott nicht auf sich selber antowrtet
             return
 
-        if message.content == "#help":
+        if message.content == "<help":
             await message.channel.send("""
             
             Ich kann nix ausser:
 
-            #help = Zeigt dir das du doofi 
-            #meinung = Ich sage dir was ich von dir halte
-            #                                         """)
-
-
-
-        if message.content.startswith("Hallo"):
-            await message.channel.send("Halloo")
+            <help = Zeigt dir das du doofi 
+            <meinung = Ich sage dir was ich von dir halte
+                                                     """)
             
 
 
-        if message.content.startswith("#meinung"):
+        if message.content.startswith("<meinung"):
             await message.channel.send("Schau in deinen privaten Nachrichten ;)")
             await message.author.send("Du willst meine Meinung wissen? \n Also, ich finde dich komisch <3")
 
 
+        if message.content.startswith("Hallo"):
+            await message.channel.send("Hallooo")
 
-    async def on_typing(self, channel, user, when):             #hört ob jemand schreibt, wenn ja passiert etc
+
+        if message.content.startswith("feierabend") or message.content.startswith("Feierabend"):
+            await message.add_reaction("âœ…")
+
+
+
+
+    async def on_typing(self, channel, user, when):             #hÃ¶rt ob jemand schreibt, wenn ja passiert etc
         print(str(user) + " tippt in " + str(channel))  
 
 
 
-    async def on_message_delete(self, message):                  #hört ob jemand was gelöscht hat, wenn ja passiert etc
+    async def on_message_delete(self, message):                  #hÃ¶rt ob jemand was gelÃ¶scht hat, wenn ja passiert etc
         print("Geloeschte Nachricht == " + str(message.content) + " von " + str(message.author))
 
 
 
-    async def on_message_edit(self, before, after):             #hört ob jemand was edited hat, wenn ja passiert etc
+    async def on_message_edit(self, before, after):             #hÃ¶rt ob jemand was edited hat, wenn ja passiert etc
         print("Eine Nachricht wurde geaendert von " + before.content + " zu " + after.content)
-
-
-
-
 
 
 
