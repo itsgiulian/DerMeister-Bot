bae0c040a83dbcf83c864a3866811516669fe830
Giuli <95866889+itsgiulian@users.noreply.github.com>
2022-11-18 21:48:41 +0100

    Bot Update
    Update, bot startet wieder

--- DerMeister_Bot.py
+++ DerMeister_Bot.py
@@ -1,47 +1,29 @@
 ﻿import discord
+from datetime import datetime
 
 
 
-
-bestatigung_itid = 902136682420240385
-bestatigungid = 902176553398575155
+#py -m pip install -U discord.py
 
 
-
-bestätigungtext = "Willkommen, drück auf den Haken um dich zu verifizieren und die AGBs zu akzeptieren"
-
-bestätigungtextIT = "Willkommen, drück auf den Haken um dich als ITler zu verifizieren und die AGBs zu akzeptieren"
-
+    
 
 
 
 class MyClient(discord.Client):
 
 
-
+    global time 
+    time = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    str(time)
+    
 
     #Einloggen in die Konsole rückmeldung
     async def on_ready(self):
-        print("Login Successfully.")
+        print(time +" Login Successfully.")
         
-        bestatigung = client.get_channel(bestatigungid)
-        await bestatigung.send(bestätigungtext)
 
-        bestatigungit = client.get_channel(bestatigung_itid)
-        await bestatigungit.send(bestätigungtextIT)
-
-
-    async def on_reaction_add(self, reaction, user):
-        friend = discord.utils.get(user.guild.roles, name="Friend")
-        if str(reaction.emoji) == "✅":
-           await user.add_roles(friend)
-
-        IT = discord.utils.get(user.guild.roles, name="IT")
-        if str(reaction.emoji) == "☑️":
-           await user.add_roles(IT)
-
-
-
+    
 
 
 
@@ -52,7 +34,7 @@ class MyClient(discord.Client):
         if message.author == client.user:       #mcacht das der Bott nicht auf sich selber antowrtet
             return
 
-
+        print(time + " Nachricht von: " + str(message.author) + " in " + str(message.channel) + ": " + str(message.content))  
 
 
         if message.content == "<help":
@@ -63,12 +45,17 @@ class MyClient(discord.Client):
             <help = Zeigt dir das du doofi 
             <meinung = Ich sage dir was ich von dir halte
                                                      """)
-            
+
+
+        if message.content.startswith("<nerv"):
+            await message.author.send("Nö")
+
 
 
         if message.content.startswith("<meinung"):
             await message.channel.send("Schau in deinen privaten Nachrichten ;)")
             await message.author.send("Du willst meine Meinung wissen? \n Also, ich finde dich komisch <3")
+            
 
 
         if message.content.startswith("Hallo"):
@@ -80,30 +67,25 @@ class MyClient(discord.Client):
 
 
 
-
-    async def on_typing(self, channel, user, when):             #hört ob jemand schreibt, wenn ja passiert etc
-        print(str(user) + " tippt in " + str(channel))  
-
+    #async def on_typing(self, channel, user, when):             #hört ob jemand schreibt, wenn ja passiert etc
+    #    print(time + " " + str(user) + " tippt in " + str(channel))  
 
 
     async def on_message_delete(self, message):                  #hört ob jemand was gelöscht hat, wenn ja passiert etc
-        print("Geloeschte Nachricht == " + str(message.content) + " von " + str(message.author))
+        print(time + " " +"Geloeschte Nachricht == " + str(message.content) + " von " + str(message.author))
 
 
 
     async def on_message_edit(self, before, after):             #hört ob jemand was edited hat, wenn ja passiert etc
-        print("Eine Nachricht wurde geaendert von " + before.content + " zu " + after.content)
+        print(time + " " +"Eine Nachricht wurde geaendert von " + before.content + " zu " + after.content)
 
 
 
 
 
-
-
-
-
-
-token = open("C:\\Users\\giuli\\Documents\\token.txt", "r")
-client = MyClient()
-client.run(token.readline())
+#token = open("C:\\Users\\giuli\\Documents\\token.txt", "r")
+intents = discord.Intents.all()
+client = MyClient(intents=intents)
+client.run("OTM5ODUwNzA0Mzk3NDM0OTUw.GBlCbs.FvpYplCKSW3aWUnqTRp3nn0RC9_FJIigME_afY")
+#client.run(token.readline())
 
